c2d0e50e13b076319abb5bcd362b2dcac82ab5f7
Ferdinand <ferdinand.loser03@gmail.com>
2026-07-30 21:29:26 +0200

    nearly finished k-means clustering (just need to add how it covers treatments)

--- Writing/main.py
+++ Writing/main.py
@@ -5,11 +5,11 @@ import sys  # <-- ADDED: This module lets us find the active Python environment
 # Define the sequence of files to execute (and their folder)
 folder = "Coding"
 scripts = [
-    'data_cleaning.py', 
+    #'data_cleaning.py', 
     #'find_optimal_hyperparameters.py', 
-    'estimation_aipw.py',
-    'propensity_score_check.py', 
-    'sensitivity_placebo.py', 
+    #'estimation_aipw.py',
+    #'propensity_score_check.py', 
+    #'sensitivity_placebo.py', 
     'sensitivity_overlap.py', 
     'tables_and_figures_econml.py', 
     'tables_and_figures_sensitivity.py'
